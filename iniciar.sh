120828b43abc6c25c83fd527ffe1839402604176
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-01-04 10:04:11 -0500

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -4,10 +4,80 @@ termux-setup-storage
 setterm -foreground red
 echo ""
 
+
 setterm -foreground red
 echo -n "[+]Presione enter para continuar _"
 read enter
+
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+
+
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ""
+echo -e "$blue(figlet)$nc ................................................... Instalado [$green✓$nc]"
+else
+sleep 1
+echo -e "$red(figlet)$nc No instalado [$red✗$nc]"
+sleep 1
+echo -e "\e[1;32mInstalando figlet ...\e[0m"
+sleep 3
+pkg install figlet
+fi
+sleep 1
+
+#directory verification
+directory=$(pwd)
+
+
+if which python >/dev/null; then
+sleep 1
+echo ""
+echo -e "$blue(python)$nc ................................................... Instalado [$green✓$nc]"
+else
+sleep 1
+echo -e "$red(python)$nc No instalado [$red✗$nc]"
+sleep 1
+echo -e "\e[1;32mInstalando python ...\e[0m"
+sleep 3
+pkg install python -y
+fi
+sleep 1
+
+#directory verification
+directory=$(pwd)
+
+
+if which python2 >/dev/null; then
+sleep 1
+echo ""
+echo -e "$blue(python2)$nc ................................................... Instalado [$green✓$nc]"
+else
+sleep 1
+echo -e "$red(python2$nc No instalado [$red✗$nc]"
+sleep 1
+echo -e "\e[1;32mInstalando python2 ...\e[0m"
+sleep 3
+pkg install python2 -y
+fi
+sleep 1
 clear
+
 figlet KitXposed
 while :
 do
